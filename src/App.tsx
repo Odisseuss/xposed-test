5e6d5a679b3c7a260f768053e55c74440343ff98
Rares <rares_lungescu@yahoo.com>
2021-01-28 01:01:05 +0200

    Add comments with my approach to implementing JWT auth

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,14 @@ import { setRecords } from "./store/slices/records";
 import { getRecordsFromApi } from "./utils/getRecordsFromApi";
 import { getAuthFromApi } from "./utils/getAuthFromApi";
 
+// Nu am mai avut timp sa implementez si functionalitatea de autorizare pe baza de JWT, dar asa as fi procedat:
+// As fi creat un endpoint in API care sa asculte pentru POST request-uri, iar atunci cand primeste unul sa creeze un JWT,
+// si un secret, si sa il atribuie unui User in baza de date (in cazul in care ar exista colectia asta).
+// Cand userul apasa pe butonul de login, se face un post request catre acest endpoint care va returna JWT-ul
+// As pastra JWT in cookies pe partea de frontend.
+// Cand userul doreste sa acceseze /form, as trimite JWT-ul din cookies catre un alt endpoint care se va ocupa cu verificare validitatii JWT-ului
+// In cazul in care JWT este valid, atunci se va trece la urmatorul pas. Daca nu e valid, se va returna un status de 403 Forbidden si un mesaj de eroare
+
 function App() {
   const dispatch = useDispatch();
   // Set logged in state for conditionally rendering pages
